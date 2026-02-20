83400e425c017489e67887222da5e03c05d6590a
Jake Mansfield <jmansfield0209@gmail.com>
2026-02-20 13:39:04 +1100

    feat: initialise query client and seed database on app load

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { seedDatabase } from './utils/seed-loader'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient()
+
+seedDatabase().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </StrictMode>
+  )
+}).catch((error) => {
+  console.error('Failed to initialise database:', error)
+})
